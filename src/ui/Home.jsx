b278bd1bd16cbebbb1e431d6afc81773e4563d49
sairu-sh <tmg.sairush@gmail.com>
2024-02-27 10:52:36 +0545

    conditionally rendered the createUser component basedn on whether the userName exists or not

--- src/ui/Home.jsx
+++ src/ui/Home.jsx
@@ -1,16 +1,26 @@
+import { useSelector } from "react-redux";
 import CreateUser from "../features/user/CreateUser";
+import Button from "./Button";
 
 function Home() {
+  const userName = useSelector((store) => store.user.userName);
+
   return (
-    <div className="my-10 text-center sm:my-16 px-4">
-      <h1 className="text-xl font-semibold mb-8 md:text-3xl">
+    <div className="my-10 px-4 text-center sm:my-16">
+      <h1 className="mb-8 text-xl font-semibold md:text-3xl">
         The best pizza.
         <br />
         <span className="text-yellow-500">
           Straight out of the oven, straight to you.
         </span>
       </h1>
-      <CreateUser />
+      {userName === "" ? (
+        <CreateUser />
+      ) : (
+        <Button to="/menu" type="primary">
+          Continue ordering, {userName}
+        </Button>
+      )}
     </div>
   );
 }
